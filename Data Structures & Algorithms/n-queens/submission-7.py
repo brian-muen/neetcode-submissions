f3f91fe701380c5cc2b971dc30e95c5638175187
brian-muen <192838847+brian-muen@users.noreply.github.com>
2026-08-19 13:16:05 -0400

    Add: n-queens - submission-7

---
+++ Data Structures & Algorithms/n-queens/submission-7.py
@@ -0,0 +1,38 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        path, res = [], []
+
+        vert = [0] * n
+        diag1 = [0] * (2 * n - 1)
+        diag2 = diag1.copy()
+
+        def backtrack(row):
+            if row == n:
+                res.append(path.copy())
+                return
+            for col in range(n):
+                d1 = row + col
+                d2 = row - col + n - 1
+                if vert[col] or diag1[d1] or diag2[d2]:
+                    continue
+                vert[col] = diag1[d1] = diag2[d2] = 1
+                path.append(self.buildString(col, n))
+                backtrack(row + 1)
+                vert[col] = diag1[d1] = diag2[d2] = 0
+                path.pop()
+
+        backtrack(0)
+        
+        return res
+
+    def buildString(self, i, n):
+        s = []
+        for j in range(n):
+            if j == i:
+                s.append("Q")
+            else:
+                s.append(".")
+
+        return "".join(s)
+
+        
